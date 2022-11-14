2d54ff84f300967cddd35a3863a65c830bc9c543
tothzalan <toth.zalan03@gmail.com>
2022-11-14 13:14:42 +0100

    Initial commit

---
+++ BinSearchTree.hs
@@ -0,0 +1,25 @@
+module BinSearchTree where
+
+data Tree a = Nil | Node a (Tree a) (Tree a)
+  deriving (Show, Eq)
+
+createNode :: a -> Tree a
+createNode a = Node a Nil Nil
+
+addTree :: Ord a => Tree a -> Tree a -> Tree a
+addTree r@(Node rv rl rr) n@(Node nv nr nl)
+  | rv > nv = Node rv n rr
+  | rv < nv = Node rv rl n
+
+addItem :: Ord a => a -> Tree a -> Tree a
+addItem a root@(Node v l r)
+  | v == a = error "v == a"
+  | v > a = recL a root root
+  | v < a = recR a root root
+  where
+    recL a acc curr@(Node v l r)
+      | l == Nil  = addTree acc (createNode a)
+      | otherwise = addTree acc (addItem a l)
+    recR a acc curr@(Node v l r)
+      | r == Nil  = addTree acc (createNode a)
+      | otherwise = addTree acc (addItem a r)
